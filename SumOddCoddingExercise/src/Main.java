2cb55a4063e7e38a1ed815cfba8a8844e9ed90bb
MariyqZlateva <mimitooo4@gmail.com>
2022-01-19 14:41:06 +0200

    Added Sum odd coding exercise

---
+++ SumOddCoddingExercise/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SumOdd.isOdd(22));
+
+        System.out.println(SumOdd.sumOdd(1, 100));
+        System.out.println(SumOdd.sumOdd(-1, 100));
+        System.out.println(SumOdd.sumOdd(100, 100));
+        System.out.println(SumOdd.sumOdd(13, 13));
+        System.out.println(SumOdd.sumOdd(100, -100));
+        System.out.println(SumOdd.sumOdd(100, 1000));
+
+    }
+}
