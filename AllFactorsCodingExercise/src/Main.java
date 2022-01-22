1a411956290b8e460c41f796ede90720a0b4e37a
MariyqZlateva <mimitooo4@gmail.com>
2022-01-22 14:46:04 +0200

    Added All factors coding exercise

---
+++ AllFactorsCodingExercise/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        //            * printFactors(6); → should print 1 2 3 6
+//            * printFactors(32); → should print 1 2 4 8 16 32
+//            * printFactors(10); → should print 1 2 5 10
+//            * printFactors(-1); → should print "Invalid Value" since number is < 1
+
+
+        AllFactors.printFactors(6);
+        System.out.println("____________________");
+        AllFactors.printFactors(32);
+        System.out.println("____________________");
+        AllFactors.printFactors(10);
+        System.out.println("____________________");
+        AllFactors.printFactors(-1);
+
+    }
+
+}
