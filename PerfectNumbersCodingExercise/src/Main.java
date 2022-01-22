49e5aa0eaab7d94b21728af3acd0d55e3b00abc9
MariyqZlateva <mimitooo4@gmail.com>
2022-01-22 15:12:19 +0200

    Added Perfect number coding exercise

---
+++ PerfectNumbersCodingExercise/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        //            * isPerfectNumber(6);
+//            should return true since its proper divisors are 1, 2, 3 and the sum is 1 + 2 + 3 = 6
+//            * isPerfectNumber(28);
+//            should return true since its proper divisors are 1, 2, 4, 7, 14
+//            and the sum is 1 + 2 + 4 + 7 + 14 = 28
+//            * isPerfectNumber(5);
+//            should return false since its only proper divisor is 1 and the sum is 1 not 5
+//            * isPerfectNumber(-1);
+//            should return false since the number is < 1
+
+
+     //   System.out.println(PerfectNumber.isPerfectNumber(6));
+        //System.out.println(PerfectNumber.isPerfectNumber(28));
+          System.out.println(PerfectNumber.isPerfectNumber(5));
+        //    System.out.println(PerfectNumber.isPerfectNumber(-1));
+    }
+}
