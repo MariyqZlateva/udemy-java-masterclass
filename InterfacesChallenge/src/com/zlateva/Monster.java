536a09fe4c8a284d486d6a0ee562da4d709cd669
MariyqZlateva <mimitooo4@gmail.com>
2022-07-25 13:55:35 +0300

    Added interfaces challenge

---
+++ InterfacesChallenge/src/com/zlateva/Monster.java
@@ -0,0 +1,69 @@
+package com.zlateva;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable {
+    private String name;
+    private int points;
+    private int strength;
+
+    public Monster(String name, int points, int strength) {
+        this.name = name;
+        this.points = points;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values = new ArrayList<>();
+        values.add(0, this.name);
+        values.add(1, "" + this.points);
+        values.add(2, "" + this.strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.points = Integer.parseInt(savedValues.get(1));
+            this.strength = Integer.parseInt(savedValues.get(2));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", points=" + points +
+                ", strength=" + strength +
+                '}';
+    }
+
+
+}
