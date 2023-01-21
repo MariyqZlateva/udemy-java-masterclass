e8ce8710bc85ceb6fe4d9110bdff975b1f303d85
MariyqZlateva <mimitooo4@gmail.com>
2023-01-21 18:43:19 +0200

    Added switch challenge solution(old version)

--- SwitchChallenge/SwitchChallenge/src/Main.java
+++ SwitchChallenge/SwitchChallenge/src/Main.java
@@ -1,7 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        char testChar = 'A';
+
+        switch (testChar) {
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println("Letter " + testChar + " was not found");
+        }
     }
-
-
 }
