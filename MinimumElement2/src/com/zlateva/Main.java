19c7ada032a3a0c5f3a8eac3d11d97ae7a30a036
MariyqZlateva <mimitooo4@gmail.com>
2023-02-10 11:27:42 +0200

    Added minimum element

--- MinimumElement2/src/com/zlateva/Main.java
+++ MinimumElement2/src/com/zlateva/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
         System.out.println(); reverseArray(returnedArray);
         System.out.println(Arrays.toString(returnedArray) );
+
     }
 
     private static int[] readIntegers() {
