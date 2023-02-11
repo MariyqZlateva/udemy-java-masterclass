8b48b901e6d92544d470fbdf27351236fe820b69
MariyqZlateva <mimitooo4@gmail.com>
2023-02-11 16:15:14 +0200

    removed comment code

--- ArrayListChallenge2/src/com/zlateva/Main.java
+++ ArrayListChallenge2/src/com/zlateva/Main.java
@@ -9,7 +9,6 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //   printMenu();
         boolean flag = true;
 
         ArrayList<String> groceries = new ArrayList<>();
