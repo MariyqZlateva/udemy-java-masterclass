79a94711d54fccd41b17d6d564649ce03d9bc2ee
MariyqZlateva <mimitooo4@gmail.com>
2023-02-19 22:03:57 +0200

    Added Abstract class

---
+++ AbstractClass/src/com/zlateva/Fish.java
@@ -0,0 +1,28 @@
+package com.zlateva;
+
+public class Fish extends Animal {
+
+
+    public Fish(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+        if (speed.equals("slow")) {
+            System.out.println(getExplicitType() + " lazy swimming");
+        } else {
+            System.out.println(getExplicitType() + " darting frantically");
+        }
+    }
+
+    @Override
+    public void makeNoise() {
+        if (type == "GoldFish ") {
+            System.out.print("swish ");
+        } else {
+            System.out.print("splash ");
+        }
+    }
+
+}
