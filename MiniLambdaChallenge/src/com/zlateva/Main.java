84b196a62dea6549cc61aff2ac442022917bf7aa
MariyqZlateva <mimitooo4@gmail.com>
2023-04-05 14:34:19 +0300

    Added mini lambda challenge - Function interface

---
+++ MiniLambdaChallenge/src/com/zlateva/Main.java
@@ -0,0 +1,79 @@
+package com.zlateva;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Consumer<String> printWords = new Consumer<String>() {
+            @Override
+            public void accept(String sentence) {
+                String[] parts = sentence.split(" ");
+                for (String part : parts) {
+                    System.out.println(part);
+                }
+            }
+        };
+
+        Consumer<String> printWordsLambda = sentence -> {
+            String[] parts = sentence.split(" ");
+            for (String part : parts) {
+                System.out.println(part);
+            }
+        };
+
+        printWords.accept("Let's split this in to an array");
+        printWordsLambda.accept("Let's split this in to an array");
+
+        Consumer<String> printWordsForEach = sentence -> {
+            String[] parts = sentence.split(" ");
+            Arrays.asList(parts).forEach(s -> System.out.println(s));
+        };
+
+        printWordsForEach.accept("Let's split this in to an array");
+
+        Consumer<String> printWordsConcise = sentence -> {
+            Arrays.asList(sentence.split(" ")).forEach(s -> System.out.println(s));
+        };
+        printWordsConcise.accept("Let's split this in to an array");
+
+        Function<String, String> everySecondChar = source-> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        System.out.println(everySecondChar.apply("12345678900"));
+
+        UnaryOperator<String> everySecondUnary = source -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+
+    }
+
+    public static String everySecondChar(String source) {
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+
+
+}
