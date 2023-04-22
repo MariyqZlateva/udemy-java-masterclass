52498421685e7b10b02ddc5ebfabec58fe0fca1d
MariyqZlateva <mimitooo4@gmail.com>
2023-04-22 12:20:06 +0300

    Refactored Final exercise - added ExtendedPassword() and SIBTest(), make storePassword final, for security purposes

--- Final/src/com/zlateva/Main.java
+++ Final/src/com/zlateva/Main.java
@@ -2,25 +2,31 @@ package com.zlateva;
 
 public class Main {
     public static void main(String[] args) {
-        SomeClass one = new SomeClass("one");
-        SomeClass two = new SomeClass("two");
-        SomeClass three = new SomeClass("three");
+//        SomeClass one = new SomeClass("one");
+//        SomeClass two = new SomeClass("two");
+//        SomeClass three = new SomeClass("three");
+//
+//        System.out.println(one.getInstanceNumber());
+//        System.out.println(two.getInstanceNumber());
+//        System.out.println(three.getInstanceNumber());
+//
+//        System.out.println(Math.PI);
+//
+//        int passWord = 674312;
+//        Password password = new ExtendedPassword(passWord);
+//        password.storePassword();
+//
+//        password.letMeIn(5232266);
+//        password.letMeIn(97745);
+//        password.letMeIn(548888);
+//        password.letMeIn(0);
+//        password.letMeIn(-674312);
+//        password.letMeIn(674312);
 
-        System.out.println(one.getInstanceNumber());
-        System.out.println(two.getInstanceNumber());
-        System.out.println(three.getInstanceNumber());
+        System.out.println("Main method called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is "+ SIBTest.owner);
 
-        System.out.println(Math.PI);
-
-        int passWord = 674312;
-        Password password = new Password(passWord);
-        password.storePassword();
-
-        password.letMeIn(5232266);
-        password.letMeIn(97745);
-        password.letMeIn(548888);
-        password.letMeIn(0);
-        password.letMeIn(-674312);
-        password.letMeIn(674312);
     }
 }
