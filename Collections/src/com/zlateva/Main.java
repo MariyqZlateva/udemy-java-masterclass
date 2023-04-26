f4d05c9a646ae6d23551be6a858731ae249cbc34
MariyqZlateva <mimitooo4@gmail.com>
2023-04-26 14:02:21 +0300

    Added Binary Search exercise - binarySearch()

---
+++ Collections/src/com/zlateva/Main.java
@@ -0,0 +1,14 @@
+package com.zlateva;
+
+public class Main {
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+      //  theatre.getSeats();
+        if (theatre.reserveSeat("B13")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+
+}
