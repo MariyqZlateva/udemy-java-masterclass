498a9ab84505e59696ed689ab49510f46ad11886
MariyqZlateva <mimitooo4@gmail.com>
2023-04-29 14:31:26 +0300

    Refactored Adventure to immutable.

--- Adventure/src/com/zlateva/Main.java
+++ Adventure/src/com/zlateva/Main.java
@@ -6,40 +6,40 @@ import java.util.Scanner;
 
 public class Main {
     public static Map<Integer, Location> locations = new HashMap<>();
+    private static Map<String, String> vocabulary = new HashMap<>();
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
+        Map<String, Integer> tempExit = new HashMap<>();
         locations.put(0, new Location(0,
-                "You are sitting in front of a computer learning Java"));
+                "You are sitting in front of a computer learning Java", tempExit));
+        tempExit = new HashMap<>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
         locations.put(1, new Location(1,
-                "You are sitting at the end of a road before a small brick building "));
+                "You are sitting at the end of a road before a small brick building ", tempExit));
+        tempExit = new HashMap<>();
+        tempExit.put("N", 5);
         locations.put(2, new Location(2,
-                "You are at the top of the hill"));
+                "You are at the top of the hill", tempExit));
+        tempExit = new HashMap<>();
+        tempExit.put("W", 1);
         locations.put(3, new Location(3,
-                "You are inside a building, a well house for a small spring"));
+                "You are inside a building, a well house for a small spring", tempExit));
+        tempExit = new HashMap<>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
         locations.put(4, new Location(4,
-                "You are in a valley beside a stream"));
+                "You are in a valley beside a stream", tempExit));
+        tempExit = new HashMap<>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
         locations.put(5, new Location(5,
-                "You are in the forest"));
+                "You are in the forest", tempExit));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
-
-        locations.get(2).addExit("N", 5);
-
-        locations.get(3).addExit("W", 1);
-
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-
-        Map<String, String> vocabulary = new HashMap<>();
         vocabulary.put("QUIT", "Q");
         vocabulary.put("NORTH", "N");
         vocabulary.put("SOUTH", "S");
@@ -50,36 +50,31 @@ public class Main {
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
+            tempExit.remove("S");
             if (loc == 0) {
                 break;
             }
             Map<String, Integer> exits = locations.get(loc).getExits();
-            System.out.print("Available exits ");
+            System.out.print("Available exits are ");
             for (String exit : exits.keySet()) {
                 System.out.print(exit + ", ");
             }
             System.out.println();
 
             String direction = scanner.nextLine().toUpperCase();
-            if (direction.length()>1){
-               String [] words = direction.split(" ");
-               for (String word : words){
-                   if (vocabulary.containsKey(word)){
-                       direction = vocabulary.get(word);
-                       break;
-                   }
-               }
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
             }
             if (exits.containsKey(direction)) {
                 loc = exits.get(direction);
             } else {
-                System.out.println("You cannot go in that substring direction");
-            }
-
-
-            String testingString = "I am Mari";
-            if (testingString.matches("M")) {
-                System.out.println("The string contains M ");
+                System.out.println("You cannot go in that direction");
             }
         }
     }
