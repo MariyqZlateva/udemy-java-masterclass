6579263ba02c65935c8bbb10c75a24c0f04a31bd
MariyqZlateva <mimitooo4@gmail.com>
2023-04-29 15:23:56 +0300

    Refactored Adventure constructor

--- Adventure/src/com/zlateva/Main.java
+++ Adventure/src/com/zlateva/Main.java
@@ -13,7 +13,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Map<String, Integer> tempExit = new HashMap<>();
         locations.put(0, new Location(0,
-                "You are sitting in front of a computer learning Java", tempExit));
+                "You are sitting in front of a computer learning Java", null));
         tempExit = new HashMap<>();
         tempExit.put("W", 2);
         tempExit.put("E", 3);
