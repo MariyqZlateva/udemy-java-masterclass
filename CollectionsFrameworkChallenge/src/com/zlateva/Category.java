90c43f57b0dd06b2353450c6bbcab765a4b7f582
MariyqZlateva <mimitooo4@gmail.com>
2023-05-15 10:05:34 +0300

    Added Collections framework challenge

---
+++ CollectionsFrameworkChallenge/src/com/zlateva/Category.java
@@ -0,0 +1,5 @@
+package com.zlateva;
+
+public enum Category {
+    PRODUCE,DAIRY, CEREAL, MEAT, BEVERAGE
+}
---
+++ CollectionsFrameworkChallenge/src/com/zlateva/Product.java
@@ -0,0 +1,7 @@
+package com.zlateva;
+
+public record Product(String sku, String name,
+                      String mfqr, Category category) {
+
+
+}
