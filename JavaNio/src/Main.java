9a959361a10b6eefd9f880af47ae875e4610e741
MariyqZlateva <mimitooo4@gmail.com>
2023-05-30 16:14:26 +0300

    Refactored JavaNio, added RandomAccessFile which is part ot IO package

--- JavaNio/src/Main.java
+++ JavaNio/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -24,6 +25,17 @@ public class Main {
             intBuffer.flip();
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            byte[] b = new byte[outputBytes.length];
+            ra.read(b);
+            System.out.println(new String(b));
+
+            long int1 = ra.readInt();
+            long int2 = ra.readInt();
+
+            System.out.println(int1);
+            System.out.println(int2);
 
 //          FileInputStream file = new FileInputStream("data.txt");
 //          FileChannel channel = file.getChannel();
