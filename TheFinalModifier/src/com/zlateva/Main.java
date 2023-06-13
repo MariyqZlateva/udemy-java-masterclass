3409d3d1f3bb2f134bba7f23737e6d628a7a15a3
MariyqZlateva <mimitooo4@gmail.com>
2023-06-13 14:33:24 +0300

    Initial commit - Final modifier exercise

---
+++ TheFinalModifier/src/com/zlateva/Main.java
@@ -0,0 +1,11 @@
+package com.zlateva;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder tracker = new StringBuilder("Step 1 is abc");
+        Logger.logToConsole(tracker.toString());
+        tracker.append(", Step 2 is xyz.");
+        Logger.logToConsole(tracker.toString());
+        System.out.println("After logging, tracker = " + tracker);
+    }
+}
