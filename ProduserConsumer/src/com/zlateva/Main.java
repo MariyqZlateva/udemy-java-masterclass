fc7b2f95addce41f2e1dd98eb679b24c517269ab
MariyqZlateva <mimitooo4@gmail.com>
2023-06-24 18:10:44 +0300

    Refactored Producer consumer exercise - added try/finally

--- ProduserConsumer/src/com/zlateva/Main.java
+++ ProduserConsumer/src/com/zlateva/Main.java
@@ -42,8 +42,11 @@ class MyProducer implements Runnable {
             try {
                 System.out.println(color + "Adding..." + number);
                 bufferLock.lock();
-                buffer.add(number);
-                bufferLock.unlock();
+                try {
+                    buffer.add(number);
+                }finally {
+                    bufferLock.unlock();
+                }
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer was interrupted");
@@ -51,8 +54,11 @@ class MyProducer implements Runnable {
         }
         System.out.println(color + "Adding EOF and exiting...");
         bufferLock.lock();
-        buffer.add("EOF");
-        bufferLock.unlock();
+        try {
+            buffer.add("EOF");
+        }finally {
+            bufferLock.unlock();
+        }
     }
 }
 
@@ -70,18 +76,19 @@ class MyConsumer implements Runnable {
     public void run() {
         while (true) {
             bufferLock.lock();
-            if (buffer.isEmpty()) {
+            try {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
+            }finally {
                 bufferLock.unlock();
-                continue;
             }
-            if (buffer.get(0).equals(EOF)) {
-                bufferLock.unlock();
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
-            }
-            bufferLock.unlock();
         }
     }
 }
