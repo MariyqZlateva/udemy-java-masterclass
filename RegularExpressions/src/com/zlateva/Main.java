7a9c1250f8db273b423d7c61a875a4bca90fb886
MariyqZlateva <mimitooo4@gmail.com>
2023-07-17 13:55:54 +0300

    Added Regular Expression - quantifiers,the Pattern and Matcher classes

--- RegularExpressions/src/com/zlateva/Main.java
+++ RegularExpressions/src/com/zlateva/Main.java
@@ -1,5 +1,8 @@
 package com.zlateva;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -54,6 +57,23 @@ public class Main {
         System.out.println(hasWhitespace.replaceAll("\\W", "X"));
         System.out.println(hasWhitespace.replaceAll("\\b", "X"));
 
+        String thirdAlphanumericString = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe{3}", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe+", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe*", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe{2,5}", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("h+i*j", "Y"));
 
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p>This is paragraph about something.<p>");
+        htmlText.append("<p>This is another paragraph about something else.<p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is the summary.<p>");
+
+        String h2Pattern = ".*<h2>.*"; // there can be anything before and after <h2>
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
     }
 }
