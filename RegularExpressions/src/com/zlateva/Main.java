eebac10e0a55e2aacba43197bfd40918d91cf8e3
MariyqZlateva <mimitooo4@gmail.com>
2023-07-17 12:24:37 +0300

    Added Regular Expression - character classes and boundary matchers

--- RegularExpressions/src/com/zlateva/Main.java
+++ RegularExpressions/src/com/zlateva/Main.java
@@ -29,5 +29,31 @@ public class Main {
         System.out.println(alphanumeric.replaceAll("[aei]", " I replaced a letter here "));
         System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
 
+        System.out.println("harry".replaceAll("[Hh]arry", "Harry"));
+
+
+        String newAlphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+
+        // replace all, without "^ej"
+        System.out.println(newAlphanumeric.replaceAll("[^ej]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[absdef345678]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[a-fA-F3-8]", "X"));
+        // (?i) makes the regex case-insensitive
+        System.out.println(newAlphanumeric.replaceAll("(?i)[a-f3-8]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[0-9]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\d", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\D", "X"));
+
+        String hasWhitespace = "I have blanks and \ta tab, and also a newLine\n";
+        System.out.println(hasWhitespace);
+        System.out.println(hasWhitespace.replaceAll("\\s", ""));
+        System.out.println(hasWhitespace.replaceAll("\t", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\S", ""));
+        System.out.println(newAlphanumeric.replaceAll("\\w", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\w", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\W", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\b", "X"));
+
+
     }
 }
