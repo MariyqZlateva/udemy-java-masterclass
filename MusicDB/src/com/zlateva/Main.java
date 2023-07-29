7827937d7d0fbc111510a6c6e9885f39362bcc3d
MariyqZlateva <mimitooo4@gmail.com>
2023-07-29 15:21:06 +0300

    Refactored MusicDB exercise - added querySongsMetaData()

--- MusicDB/src/com/zlateva/Main.java
+++ MusicDB/src/com/zlateva/Main.java
@@ -2,6 +2,7 @@ package com.zlateva;
 
 import com.zlateva.model.Artist;
 import com.zlateva.model.Datasource;
+import com.zlateva.model.SongArtist;
 
 import java.util.List;
 
@@ -25,11 +26,25 @@ public class Main {
         }
 
         List<String> albumsForArtist =
-                datasource.queryAlbumsForArtist("Iron Maiden", Datasource.ORDER_BY_ASC);
+                datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_DESC);
 
         for (String album : albumsForArtist) {
             System.out.println(album);
         }
+
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_DESC);
+        if (songArtists == null) {
+            System.out.println("Couldn't fined the artist for the song");
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track = " + artist.getTrack());
+        }
+
+
+        datasource.querySongsMetadata();
         datasource.close();
     }
 }
