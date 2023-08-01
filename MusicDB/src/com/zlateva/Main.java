d10aea75a6d73c2a708b96b7a9277e71dcd0ce9a
MariyqZlateva <mimitooo4@gmail.com>
2023-08-01 08:51:36 +0300

    Refactored MusicDB exercise - added prepared statement

--- MusicDB/src/com/zlateva/Main.java
+++ MusicDB/src/com/zlateva/Main.java
@@ -5,6 +5,7 @@ import com.zlateva.model.Datasource;
 import com.zlateva.model.SongArtist;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,27 +15,27 @@ public class Main {
             return;
         }
 
-
-        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_DESC);
+        List<Artist> artists = datasource.queryArtists(5);
         if (artists == null) {
             System.out.println("No artists!");
             return;
         }
 
         for (Artist artist : artists) {
-            System.out.println("Id = " + artist.getId() + " Name = " + artist.getName());
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
         List<String> albumsForArtist =
-                datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_DESC);
+                datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
 
         for (String album : albumsForArtist) {
             System.out.println(album);
         }
 
-        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_DESC);
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_ASC);
         if (songArtists == null) {
-            System.out.println("Couldn't fined the artist for the song");
+            System.out.println("Couldn't find the artist for the song");
+            return;
         }
 
         for (SongArtist artist : songArtists) {
@@ -43,8 +44,30 @@ public class Main {
                     " Track = " + artist.getTrack());
         }
 
-
         datasource.querySongsMetadata();
+
+        int count = datasource.getCount(Datasource.TABLE_SONGS);
+        System.out.println("Number of songs is: " + count);
+
+        datasource.createViewForSongArtists();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a song title: ");
+        String title = scanner.nextLine();
+
+        //Go Your Own Way" ot 1=1 or" sql injection
+        songArtists = datasource.querySongInfoView(title);
+        if (songArtists.isEmpty()) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("FROM VIEW - Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track number = " + artist.getTrack());
+        }
+
         datasource.close();
     }
 }
